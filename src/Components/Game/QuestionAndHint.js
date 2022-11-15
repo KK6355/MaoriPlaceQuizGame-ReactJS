dcfac6ce2f85200fb13f23173782e0ebba129f60
Vicky_Kang <93057655+KK6355@users.noreply.github.com>
2022-11-15 19:23:25 +1300

    add dynamic google map with place name

--- src/Components/Game/QuestionAndHint.js
+++ src/Components/Game/QuestionAndHint.js
@@ -1,8 +1,17 @@
 import classes from "./QuestionAndHint.module.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 const QuestionAndHint = (props) => {
   return (
     <div className={classes.card}>
+      <a
+        href={`https://www.google.co.nz/maps/place/${props.question}/`}
+        target="_blank"
+        rel="noreferrer"
+        title="see where it is"
+      >
+        <FontAwesomeIcon icon={faLocationDot} className={classes.icon} />
+      </a>
       <p className={classes.question}>{props.question}</p>
       <p className={classes.hint}>Hint: {props.hint}</p>
     </div>
